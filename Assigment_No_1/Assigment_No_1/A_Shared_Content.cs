a634d9d519b6618bfe83f3cb8e3bb703f25be697
Anushka12005 <anushkajamdade43@gmail.com>
2025-12-11 13:57:33 +0530

    Assignment_No_1

---
+++ Assigment_No_1/Assigment_No_1/A_Shared_Content.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_No_1
+{
+    class A_Shared_Content
+    {
+        public static string LoginUsername = "";
+    }
+}
---
+++ Assigment_No_1/Assigment_No_1/frm_Student_List.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Assigment_No_1
+{
+    partial class frm_Student_List
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_Header = new System.Windows.Forms.Panel();
+            this.btn_Logout = new System.Windows.Forms.Button();
+            this.lbl_Student_List = new System.Windows.Forms.Label();
+            this.btn_Add_Student = new System.Windows.Forms.Button();
+            this.dgv_Add_Student = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pnl_Header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Add_Student)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_Header
+            // 
+            this.pnl_Header.BackColor = System.Drawing.Color.LightCyan;
+            this.pnl_Header.Controls.Add(this.btn_Logout);
+            this.pnl_Header.Controls.Add(this.lbl_Student_List);
+            this.pnl_Header.Location = new System.Drawing.Point(1, 1);
+            this.pnl_Header.Name = "pnl_Header";
+            this.pnl_Header.Size = new System.Drawing.Size(839, 135);
+            this.pnl_Header.TabIndex = 0;
+            // 
+            // btn_Logout
+            // 
+            this.btn_Logout.BackColor = System.Drawing.Color.DeepPink;
+            this.btn_Logout.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Logout.Location = new System.Drawing.Point(721, 11);
+            this.btn_Logout.Name = "btn_Logout";
+            this.btn_Logout.Size = new System.Drawing.Size(107, 36);
+            this.btn_Logout.TabIndex = 1;
+            this.btn_Logout.Text = "Logout";
+            this.btn_Logout.UseVisualStyleBackColor = false;
+            // 
+            // lbl_Student_List
+            // 
+            this.lbl_Student_List.AutoSize = true;
+            this.lbl_Student_List.BackColor = System.Drawing.Color.White;
+            this.lbl_Student_List.Font = new System.Drawing.Font("Mongolian Baiti", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Student_List.Location = new System.Drawing.Point(285, 39);
+            this.lbl_Student_List.Name = "lbl_Student_List";
+            this.lbl_Student_List.Size = new System.Drawing.Size(216, 40);
+            this.lbl_Student_List.TabIndex = 0;
+            this.lbl_Student_List.Text = "Student List";
+            // 
+            // btn_Add_Student
+            // 
+            this.btn_Add_Student.BackColor = System.Drawing.Color.DeepPink;
+            this.btn_Add_Student.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add_Student.Location = new System.Drawing.Point(293, 544);
+            this.btn_Add_Student.Name = "btn_Add_Student";
+            this.btn_Add_Student.Size = new System.Drawing.Size(166, 52);
+            this.btn_Add_Student.TabIndex = 2;
+            this.btn_Add_Student.Text = "Add Student";
+            this.btn_Add_Student.UseVisualStyleBackColor = false;
+            // 
+            // dgv_Add_Student
+            // 
+            this.dgv_Add_Student.AllowUserToAddRows = false;
+            this.dgv_Add_Student.AllowUserToDeleteRows = false;
+            this.dgv_Add_Student.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Add_Student.BackgroundColor = System.Drawing.Color.White;
+            this.dgv_Add_Student.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Add_Student.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgv_Add_Student.Location = new System.Drawing.Point(1, 136);
+            this.dgv_Add_Student.Name = "dgv_Add_Student";
+            this.dgv_Add_Student.ReadOnly = true;
+            this.dgv_Add_Student.RowHeadersWidth = 51;
+            this.dgv_Add_Student.RowTemplate.Height = 24;
+            this.dgv_Add_Student.Size = new System.Drawing.Size(839, 402);
+            this.dgv_Add_Student.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Roll No";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Date of Birth";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Mob No";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Course";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // frm_Student_List
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(841, 608);
+            this.Controls.Add(this.dgv_Add_Student);
+            this.Controls.Add(this.btn_Add_Student);
+            this.Controls.Add(this.pnl_Header);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Student_List";
+            this.Text = "Student List";
+            this.Load += new System.EventHandler(this.frm_Student_List_Load);
+            this.pnl_Header.ResumeLayout(false);
+            this.pnl_Header.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Add_Student)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Header;
+        private System.Windows.Forms.Label lbl_Student_List;
+        private System.Windows.Forms.Button btn_Logout;
+        private System.Windows.Forms.Button btn_Add_Student;
+        private System.Windows.Forms.DataGridView dgv_Add_Student;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
